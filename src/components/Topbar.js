3081fb89956d57194132c9a8ca1e8077ec150877
chrisco210 <chrisrachlinski@gmail.com>
2020-07-14 20:16:33 -0400

    Add color changing navbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,17 +1,25 @@
 import React from "react"
 import { AppBar } from "@material-ui/core"
 import { Toolbar } from "@material-ui/core"
-import Theme from "../Theme"
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
 
 export default function Topbar(props) {
+  let trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
   return (
-    <Theme>
-      <AppBar position="fixed">
+    <React.Fragment>
+      <AppBar position="fixed" color={trigger ? "default" : "primary"}
+        elevation={trigger ? 2 : 0}
+        style={{ transition: "400ms ease" }}>
         <Toolbar>
           <p>Test</p>
         </Toolbar>
       </AppBar>
       <Toolbar />
-    </Theme>
+    </React.Fragment >
   );
 };
